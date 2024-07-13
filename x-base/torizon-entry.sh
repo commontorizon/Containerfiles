f441269944b0fa563f0824e0c11d830f03ac6b6a
Matheus Castello <matheus@castello.eng.br>
2024-07-13 18:33:00 -0300

    x-base: Add base for run Xorg applications
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

---
+++ x-base/torizon-entry.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# exits immediately if any error has non-zero exit
+set -e
+# error the whole pipeline if a subcommand fails
+set -o pipefail
+
+export XDG_RUNTIME_DIR=/tmp/xdg
+export X11_UNIX_SOCKET="/tmp/.X11-unix"
+export XDG_RUNTIME_DIR=/tmp/$(id -u torizon)-runtime-dir
+export XAUTHORITY=/tmp/.Xauthority
+export DBUS_SESSION_BUS_ADDRESS=unix:path=/tmp/dbus-session-bus
+
+function init()
+{
+        # echo error message, when executable file doesn't exist.
+        if CMD=$(command -v "$1" 2>/dev/null); then
+                shift
+                export DISPLAY=:0
+
+                exec "$CMD" "$@"
+        else
+                echo "Command not found: $1"
+
+                # houston we have a problem
+                exit 1
+        fi
+}
+
+init $@
