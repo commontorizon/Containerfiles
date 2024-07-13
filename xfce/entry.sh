96eea6f4dde10a7b3251ab2bb57fd47ce04295ee
Matheus Castello <matheus@castello.eng.br>
2024-07-13 16:51:12 -0300

    xfce: Add xfce kiosk image
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

---
+++ xfce/entry.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+
+# exits immediately if any error has non-zero exit
+set -e
+# error the whole pipeline if a subcommand fails
+set -o pipefail
+
+# use tty7 for the graphical server by default
+VT="7"
+
+if [[ $? == 0 ]]; then
+	PRIVILEGED=true
+else
+	PRIVILEGED=false
+fi
+
+function init_xdg() {
+	if test -z "${XDG_RUNTIME_DIR}"; then
+		XDG_RUNTIME_DIR=/tmp/$(id -u torizon)-runtime-dir
+		export XDG_RUNTIME_DIR
+	fi
+
+	echo "XDG_RUNTIME_DIR=${XDG_RUNTIME_DIR}" >>/etc/environment
+
+	if ! test -d "${XDG_RUNTIME_DIR}"; then
+		mkdir -p "${XDG_RUNTIME_DIR}"
+	fi
+
+	chown torizon "${XDG_RUNTIME_DIR}"
+	chmod 0700 "${XDG_RUNTIME_DIR}"
+
+	# Create folder for XWayland Unix socket
+	export X11_UNIX_SOCKET="/tmp/.X11-unix"
+	if ! test -d "${X11_UNIX_SOCKET}"; then
+		mkdir -p ${X11_UNIX_SOCKET}
+	fi
+
+	chown torizon:video ${X11_UNIX_SOCKET}
+}
+
+# the base stuff does not need to run privileged
+function start_udev()
+{
+	if [ "$UDEV" == "on" ]; then
+		if $PRIVILEGED; then
+			if command -v udevd &>/dev/null; then
+				unshare --net udevd --daemon &> /dev/null
+			else
+				unshare --net /lib/systemd/systemd-udevd --daemon &> /dev/null
+			fi
+			udevadm trigger &> /dev/null
+		else
+			echo "Unable to start udev, container must be run in privileged mode to start udev!"
+		fi
+	fi
+}
+
+function config_tty()
+{
+        # 🤨
+        chmod 666 /dev/tty*
+}
+
+function vt_setup() {
+	# Some applications may leave old VT in graphics mode which causes
+	# applications like openvt and chvt to hang at VT_WAITACTIVE ioctl when they
+	# try to switch to a new VT
+
+	# grabs the current active VT, before possibly switching
+	OLD_VT=$(cat /sys/class/tty/tty0/active)
+	OLD_VT_MODE=$(kbdinfo -C /dev/"${OLD_VT}" getmode)
+	if [ "$OLD_VT_MODE" = "graphics" ]; then
+		/usr/bin/switchvt.pl "${OLD_VT:3}" text
+	fi
+}
+
+UDEV=$(echo "$UDEV" | awk '{print tolower($0)}')
+
+case "$UDEV" in
+        '1' | 'true')
+                UDEV='on'
+        ;;
+esac
+
+# remove old dbus session bus
+echo "Removing old dbus session bus ..."
+rm -rf /tmp/dbus-session-bus
+
+# remove the old x unix socket
+echo "Removing old X11 unix socket ..."
+rm -rf /tmp/.X*
+
+config_tty
+echo "Switching VT $(cat /sys/class/tty/tty0/active) to text mode if currently in graphics mode" && vt_setup
+echo "Switching to VT ${VT}" && chvt "${VT}"
+
+start_udev
+init_xdg
+
+# get all the arguments from this script and put in a variable
+# so we can pass it to the torizon-entry.sh
+ARGS="$@"
+export ARGS
+
+# now we can execute the torizon-entry.sh
+# as torizon user
+su torizon -c "/usr/bin/torizon-entry.sh $ARGS"
